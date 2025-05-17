f867ed561b1ea94f9e8311ae24a95429b5bdcf13
brayan barrera <brayanstbv@gmail.com>
2025-05-16 22:28:54 -0500

    creation of main programs and classes

---
+++ DobleList/DobleList/DoublyLinkedList.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoubleList;
+
+public class DoublyLinkedList<T>
+
+{
+    private DoubleNode<T>? _head;
+    private DoubleNode<T>? _tail;
+
+    public DoublyLinkedList()
+    {
+        _tail = null;
+        _head = null;
+    }
+
+    public void InserAtBeginning(T data)
+    {
+        var newNode = new DoubleNode<T>(data);
+        if (_head == null)// Empty list
+        {
+            _head = newNode;
+            _tail = newNode;
+        }
+        else
+        {
+            newNode.Next = _head;
+            _head.Prev = newNode;
+            _head = newNode;
+
+        }
+
+    }
+
+    public void InserAtEnd(T data)
+    {
+        var newNode = new DoubleNode<T>(data);
+        if (_tail == null)
+        {
+            _head = newNode;
+            _tail = newNode;
+        }
+        else
+        {
+            _tail.Next = newNode;
+            newNode.Prev = _tail;
+            _tail = newNode;
+        }
+    }
+    //Prints the List fron head to tail
+    public String GettForward()
+    {
+        var output = String.Empty;
+        var current = _head;
+        while (current != null)
+        {
+            output += $"{current.Data}<=>";
+            current = current.Next;
+        }
+        return output;
+    }
+
+    //Prints the List fron head to head
+    public String GetBackward()
+    {
+        var output = String.Empty;
+        var current = _tail;
+        while (current != null)
+        {
+            output += $"{current.Data} <=> ";
+            current = current.Prev;
+        }
+        return output.Substring(0, output.Length - 5);
+    }
+
+    public void Remove(T data)
+    {
+        var current = _head;
+        while (current != null)
+        {
+            if (current.Data!.Equals(data))
+            {
+                if (current.Prev != null)
+                {
+                    current.Prev.Next = current.Next;
+                }
+                else
+                {
+                    _head = current.Next; // Remuve head
+                }
+
+                if (current.Next != null)
+                {
+                    current.Next.Prev = current.Prev;
+                }
+                else
+                {
+                    _tail = current.Prev;// Remuve tail
+                }
+                break;
+            }
+            current = current.Next;
+        }
+
+
+
+    }
+
+}
---
+++ DobleList/DoubleList.UI.Console/Program.cs
@@ -0,0 +1,64 @@
+﻿using DoubleList;
+using System.ComponentModel.Design;
+
+var List = new DoublyLinkedList<string>();
+var opc = "0";
+
+do
+{
+    opc = Menu();
+    switch (opc)
+
+    {
+        case "1":
+            Console.Write("Enter the data to insert at the beginning: ");
+            var dataAtBeginning = Console.ReadLine();
+            if (dataAtBeginning != null)
+            {
+                List.InserAtBeginning(dataAtBeginning);
+            }
+            break;
+        case "2":
+            Console.Write("Enter the data to insert at the end:");
+            var dataAtEnd = Console.ReadLine();
+            if (dataAtEnd != null)
+            {
+                List.InserAtEnd(dataAtEnd);
+            }
+            break;
+
+        case "3":
+            Console.WriteLine(List.GettForward());
+            break;
+
+        case "4":
+            Console.WriteLine(List.GetBackward());
+            break;
+
+        case "5":
+            Console.WriteLine("Enter the data to remove: ");
+            var remove = Console.ReadLine();
+            if (remove != null)
+            {
+                List.Remove(remove);
+                Console.WriteLine("Item removed.");
+            }
+            break;
+
+    }
+}
+while (opc != "0");
+
+string Menu()
+{
+    Console.WriteLine("1. Insert at beginning");
+    Console.WriteLine("2. Insert at end");
+    Console.WriteLine("3. Show list forward");
+    Console.WriteLine("4. Show list backward");
+    Console.WriteLine("5. Remove");
+    Console.WriteLine("0. Exit");
+    Console.Write("Choose an option: ");
+    return Console.ReadLine() ?? "0";
+}
+
+
